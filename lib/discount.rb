4a21e1614837d3aed8e4a0b6465e3a609443ccc2
benjaminsunderland <ben@sunderland.eu.com>
2022-01-17 21:32:23 +0000

    refactor discount class

--- lib/discount.rb
+++ lib/discount.rb
@@ -1,31 +1,31 @@
 class Discount
 
-    attr_reader :prices, :total
+    attr_reader :prices
+    attr_writer :total
 
-    def initialize(prices, total)
-        @prices = prices
-        @total = total
+    def initialize(prices)
+       @prices = prices
     end
 
-    def apple_pear_discount(item)
-        @total += @prices.fetch(item) * (count / 2)
+    def apple_pear_discount(item, count)
+       @prices.fetch(item) * (count / 2)
     end
 
-    def pineapple_discount(item)
-        @total += (@prices.fetch(item) / 2)
-        @total += (@prices.fetch(item)) * (count - 1)
+    def pineapple_discount(item, count)
+       (@prices.fetch(item) / 2)
+       (@prices.fetch(item)) * (count - 1)
     end
 
-    def banana_discount(item)
-        (@prices.fetch(item) / 2) * count
+    def banana_discount(item, count)
+       (@prices.fetch(item) / 2) * count
     end
 
-    def mango_discount(item)
-        @total += (prices.fetch(item) * count * 0.75).to_i
+    def mango_discount(item, count)
+       (@prices.fetch(item) * count * 0.75).to_i
     end
 
-    def base_price(item)
-        @total += @prices.fetch(item) * count
+    def base_price(item, count)
+       @prices.fetch(item) * count
     end
 
   end
