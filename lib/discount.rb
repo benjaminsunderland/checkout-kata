2655f188774b5377c30440db3e4a3e2b8733f222
benjaminsunderland <ben@sunderland.eu.com>
2022-01-17 21:52:23 +0000

    rename variable

--- lib/discount.rb
+++ lib/discount.rb
@@ -3,33 +3,32 @@ class Discount
     attr_reader :prices
     attr_writer :total
 
-    def initialize(prices)
-       @prices = prices
+    def initialize(item_prices)
+       @item_prices = item_prices
     end
 
     def apple_pear_discount(item, count)
         if (count % 2 == 0)
-            @prices.fetch(item) * (count / 2)
+            @item_prices.fetch(item) * (count / 2)
           else
-            @prices.fetch(item) * count
+            @item_prices.fetch(item) * count
         end
     end
 
     def pineapple_discount(item, count)
-       (@prices.fetch(item) / 2)
-       (@prices.fetch(item)) * (count - 1)
+       (@item_prices.fetch(item)) * (count - 1)
     end
 
     def banana_discount(item, count)
-       (@prices.fetch(item) / 2) * count
+       (@item_prices.fetch(item) / 2) * count
     end
 
     def mango_discount(item, count)
-       (@prices.fetch(item) * count * 0.75).to_i
+       (@item_prices.fetch(item) * count * 0.75).to_i
     end
 
     def base_price(item, count)
-       @prices.fetch(item) * count
+       @item_prices.fetch(item) * count
     end
 
   end
